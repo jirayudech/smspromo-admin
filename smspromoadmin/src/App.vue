bdf7a2b51e9cea7121163488a31db1872945f522
Jirayudech <jirayudech@Jirayudechs-MacBook-Pro.local>
2018-03-17 09:40:06 +0700

    clean code

--- smspromoadmin/src/App.vue
+++ smspromoadmin/src/App.vue
@@ -1,65 +1,14 @@
 <template>
   <v-app>
-<!--     <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile 
-          value="true"
-          v-for="(item, i) in menuItems"
-          :key="i"
-          :to="item.link"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer> -->
     <navigation></navigation>
-<!--     <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="appName+title"></v-toolbar-title>
-      <v-spacer></v-spacer>
 
-
-    <v-btn flat @click="userSignOut" v-if="isAuthenticated">
-      <v-icon left>exit_to_app</v-icon>
-      Sign Out
-    </v-btn>
-    <v-btn
-      flat
-      v-for="(item, i) in toolbarItems"
-      :key="item.i"
-      :to="item.link">
-      <v-icon left>{{ item.icon }}</v-icon>
-      {{ item.title }}
-    </v-btn>
-
-    </v-toolbar> -->
     <v-content>
       <v-container fluid>
         <!-- Redered Content Show Here. -->
         <router-view></router-view> 
       </v-container>
     </v-content>
+    
     <v-navigation-drawer
       temporary
       :right="right"
